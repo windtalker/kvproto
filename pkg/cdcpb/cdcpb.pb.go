fd9325002cc19ae14530acda68350c150570aff9
Neil Shen <overvenus@gmail.com>
2019-10-29 15:46:18 +0800

    cdc: turn checkpoint_ts into uint64
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- pkg/cdcpb/cdcpb.pb.go
+++ pkg/cdcpb/cdcpb.pb.go
@@ -66,7 +66,7 @@ func (x Event_LogType) String() string {
 	return proto.EnumName(Event_LogType_name, int32(x))
 }
 func (Event_LogType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 0}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 0}
 }
 
 type Event_Row_OpType int32
@@ -92,7 +92,7 @@ func (x Event_Row_OpType) String() string {
 	return proto.EnumName(Event_Row_OpType_name, int32(x))
 }
 func (Event_Row_OpType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 0, 0}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 0, 0}
 }
 
 type Header struct {
@@ -106,7 +106,7 @@ func (m *Header) Reset()         { *m = Header{} }
 func (m *Header) String() string { return proto.CompactTextString(m) }
 func (*Header) ProtoMessage()    {}
 func (*Header) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{0}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{0}
 }
 func (m *Header) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -160,7 +160,7 @@ func (m *Event) Reset()         { *m = Event{} }
 func (m *Event) String() string { return proto.CompactTextString(m) }
 func (*Event) ProtoMessage()    {}
 func (*Event) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1}
 }
 func (m *Event) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -385,7 +385,7 @@ func (m *Event_Row) Reset()         { *m = Event_Row{} }
 func (m *Event_Row) String() string { return proto.CompactTextString(m) }
 func (*Event_Row) ProtoMessage()    {}
 func (*Event_Row) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 0}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 0}
 }
 func (m *Event_Row) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -467,7 +467,7 @@ func (m *Event_Entries) Reset()         { *m = Event_Entries{} }
 func (m *Event_Entries) String() string { return proto.CompactTextString(m) }
 func (*Event_Entries) ProtoMessage()    {}
 func (*Event_Entries) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 1}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 1}
 }
 func (m *Event_Entries) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -515,7 +515,7 @@ func (m *Event_Admin) Reset()         { *m = Event_Admin{} }
 func (m *Event_Admin) String() string { return proto.CompactTextString(m) }
 func (*Event_Admin) ProtoMessage()    {}
 func (*Event_Admin) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 2}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 2}
 }
 func (m *Event_Admin) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -571,7 +571,7 @@ func (m *Event_Error) Reset()         { *m = Event_Error{} }
 func (m *Event_Error) String() string { return proto.CompactTextString(m) }
 func (*Event_Error) ProtoMessage()    {}
 func (*Event_Error) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{1, 3}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{1, 3}
 }
 func (m *Event_Error) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -632,7 +632,7 @@ func (m *ChangeDataEvent) Reset()         { *m = ChangeDataEvent{} }
 func (m *ChangeDataEvent) String() string { return proto.CompactTextString(m) }
 func (*ChangeDataEvent) ProtoMessage()    {}
 func (*ChangeDataEvent) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{2}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{2}
 }
 func (m *ChangeDataEvent) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -672,7 +672,7 @@ type ChangeDataRequest struct {
 	Header               *Header             `protobuf:"bytes,1,opt,name=header" json:"header,omitempty"`
 	RegionId             uint64              `protobuf:"varint,2,opt,name=region_id,json=regionId,proto3" json:"region_id,omitempty"`
 	RegionEpoch          *metapb.RegionEpoch `protobuf:"bytes,3,opt,name=region_epoch,json=regionEpoch" json:"region_epoch,omitempty"`
-	CheckpointTs         int64               `protobuf:"varint,4,opt,name=checkpoint_ts,json=checkpointTs,proto3" json:"checkpoint_ts,omitempty"`
+	CheckpointTs         uint64              `protobuf:"varint,4,opt,name=checkpoint_ts,json=checkpointTs,proto3" json:"checkpoint_ts,omitempty"`
 	StartKey             []byte              `protobuf:"bytes,5,opt,name=start_key,json=startKey,proto3" json:"start_key,omitempty"`
 	EndKey               []byte              `protobuf:"bytes,6,opt,name=end_key,json=endKey,proto3" json:"end_key,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}            `json:"-"`
@@ -684,7 +684,7 @@ func (m *ChangeDataRequest) Reset()         { *m = ChangeDataRequest{} }
 func (m *ChangeDataRequest) String() string { return proto.CompactTextString(m) }
 func (*ChangeDataRequest) ProtoMessage()    {}
 func (*ChangeDataRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_cdcpb_786dabf62dd44af1, []int{3}
+	return fileDescriptor_cdcpb_ce648446a19a5c36, []int{3}
 }
 func (m *ChangeDataRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -734,7 +734,7 @@ func (m *ChangeDataRequest) GetRegionEpoch() *metapb.RegionEpoch {
 	return nil
 }
 
-func (m *ChangeDataRequest) GetCheckpointTs() int64 {
+func (m *ChangeDataRequest) GetCheckpointTs() uint64 {
 	if m != nil {
 		return m.CheckpointTs
 	}
@@ -2490,7 +2490,7 @@ func (m *ChangeDataRequest) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.CheckpointTs |= (int64(b) & 0x7F) << shift
+				m.CheckpointTs |= (uint64(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
@@ -2684,9 +2684,9 @@ var (
 	ErrIntOverflowCdcpb   = fmt.Errorf("proto: integer overflow")
 )
 
-func init() { proto.RegisterFile("cdcpb.proto", fileDescriptor_cdcpb_786dabf62dd44af1) }
+func init() { proto.RegisterFile("cdcpb.proto", fileDescriptor_cdcpb_ce648446a19a5c36) }
 
-var fileDescriptor_cdcpb_786dabf62dd44af1 = []byte{
+var fileDescriptor_cdcpb_ce648446a19a5c36 = []byte{
 	// 823 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x54, 0xcd, 0x6e, 0xdb, 0x46,
 	0x10, 0x16, 0x2d, 0x91, 0x92, 0x86, 0x92, 0xcd, 0x6e, 0x8d, 0x98, 0x51, 0x51, 0xc3, 0x55, 0xff,
@@ -2732,12 +2732,12 @@ var fileDescriptor_cdcpb_786dabf62dd44af1 = []byte{
 	0xe0, 0x9d, 0x75, 0x65, 0xe5, 0xdc, 0xc7, 0x60, 0x4d, 0x37, 0x95, 0xee, 0x96, 0xb5, 0xfa, 0x45,
 	0xa1, 0x25, 0xb8, 0xfd, 0x60, 0xec, 0xdc, 0x7b, 0x30, 0xbe, 0x82, 0x4e, 0x09, 0xa2, 0x26, 0xa5,
 	0x70, 0xef, 0x7a, 0xe5, 0xdb, 0xa6, 0x95, 0x47, 0xf5, 0xa8, 0x2d, 0xd6, 0x01, 0xf9, 0x10, 0xba,
-	0xf1, 0x94, 0xc5, 0x37, 0x39, 0x4f, 0x33, 0x5c, 0x43, 0xb5, 0x41, 0x75, 0xda, 0x59, 0x27, 0xf5,
-	0x9e, 0xea, 0x15, 0x5e, 0x2f, 0x8d, 0xde, 0xe9, 0x4b, 0xb6, 0x24, 0x07, 0x6a, 0xd8, 0x13, 0x84,
-	0xf4, 0xee, 0x58, 0x2c, 0x4b, 0x2e, 0xd9, 0x72, 0x70, 0x05, 0xb0, 0xee, 0x95, 0x7c, 0x07, 0x6d,
-	0xd4, 0xe2, 0x09, 0x63, 0x09, 0x71, 0xcb, 0x0e, 0xdf, 0xd0, 0xa2, 0xf7, 0xe0, 0x0d, 0x04, 0xab,
-	0x1e, 0x1b, 0xa7, 0x9f, 0xfc, 0xfd, 0x5b, 0xcb, 0xf8, 0xf3, 0xee, 0xd0, 0xf8, 0xeb, 0xee, 0xd0,
-	0xf8, 0xe7, 0xee, 0xd0, 0xf8, 0xf5, 0xdf, 0xc3, 0x1a, 0x38, 0x5c, 0x4c, 0x3c, 0x99, 0xde, 0x2c,
-	0xbc, 0x9b, 0x05, 0x3e, 0xc5, 0x2f, 0x2c, 0xfc, 0x7c, 0xf1, 0x3a, 0x00, 0x00, 0xff, 0xff, 0x68,
-	0xd0, 0x0b, 0xed, 0xfd, 0x05, 0x00, 0x00,
+	0xf1, 0x94, 0xc5, 0x37, 0x39, 0x4f, 0x33, 0x5c, 0xc3, 0x06, 0xfe, 0xb8, 0xb3, 0x4e, 0xea, 0x3d,
+	0xd5, 0x2b, 0xbc, 0x5e, 0x1a, 0xbd, 0xd3, 0x97, 0x6c, 0x49, 0x0e, 0xd4, 0xb0, 0x27, 0x08, 0xe9,
+	0xdd, 0xb1, 0x58, 0x96, 0x5c, 0xb2, 0xe5, 0xe0, 0x0a, 0x60, 0xdd, 0x2b, 0xf9, 0x0e, 0xda, 0xa8,
+	0xc5, 0x13, 0xc6, 0x12, 0xe2, 0x96, 0x1d, 0xbe, 0xa1, 0x45, 0xef, 0xc1, 0x1b, 0x08, 0x56, 0x3d,
+	0x36, 0x4e, 0x3f, 0xf9, 0xfb, 0xb7, 0x96, 0xf1, 0xe7, 0xdd, 0xa1, 0xf1, 0xd7, 0xdd, 0xa1, 0xf1,
+	0xcf, 0xdd, 0xa1, 0xf1, 0xeb, 0xbf, 0x87, 0x35, 0x70, 0xb8, 0x98, 0x78, 0x32, 0xbd, 0x59, 0x78,
+	0x37, 0x0b, 0x7c, 0x8a, 0x5f, 0x58, 0xf8, 0xf9, 0xe2, 0x75, 0x00, 0x00, 0x00, 0xff, 0xff, 0x6b,
+	0x3b, 0xbc, 0x01, 0xfd, 0x05, 0x00, 0x00,
 }
